0ab4e4d912fd2f2e9edb2b4253af0fd7000d2556
Mustafa Hakan Orskaya <mhorskaya@hotmail.com>
2021-10-21 17:40:47 +0300

    set allowed header

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
 		w.WriteHeader(405)
 		w.Write([]byte("Method Not Allowed"))
 		return
